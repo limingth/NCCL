1e4448d284b2fed9fa6a7873192e4179bc6195d8
Li Ming <limingth@gmail.com>
2012-11-02 18:23:11 +0800

    Update Unit-1/Lesson-2/lesson-2-3.c

--- Unit-1/Lesson-2/lesson-2-3.c
+++ Unit-1/Lesson-2/lesson-2-3.c
@@ -1,15 +1,23 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 static void foo(void) __attribute__ ((constructor));
 static void bar(void) __attribute__ ((destructor));
 
-int main(int argc, char *argv[])
+void test(void)
 {
+    printf("<test> at exit \n");
+}
+
+int main(int argc, char *argv[])
+{	
 	printf("<main> execution\n", foo);
+
 	printf("foo == %p\n", foo);
 	printf("bar == %p\n", bar);
 
+	atexit(test);    
 	exit(EXIT_SUCCESS);
 }
 
